72b20d55ef1b822b3fcc2b1c5ff35680df10ea60
spleekz <Shutnik-12K@yandex,ru>
2021-09-24 14:48:03 +0300

    add root store context

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
+import { RootStoreProvider } from './stores/RootStore/RootStoreContext'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <RootStoreProvider>
+      <App />
+    </RootStoreProvider>
   </BrowserRouter>,
   document.getElementById('root')
 )
