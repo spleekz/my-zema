acb4a864a8e3c5ae2f7bddced82ea1b43097fcf6
spleekz <Shutnik-12K@yandex,ru>
2021-09-24 19:49:36 +0300

    change body bgc

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const useStyles = makeStyles(() =>
       body: {
         padding: 0,
         margin: 0,
-        backgroundColor: '#95A3B3',
+        backgroundColor: '#f3f3f3',
         fontFamily: 'Alegreya Sans SC, sans-serif !important',
       },
       '#root': {
