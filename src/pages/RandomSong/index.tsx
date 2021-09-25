7b12fa0b5523a8e31fadfa28b3b2d2d7c55cf63f
spleekz <Shutnik-12K@yandex,ru>
2021-09-25 21:52:28 +0300

    render track filters

--- src/pages/RandomSong/index.tsx
+++ src/pages/RandomSong/index.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
+import { RadioGroup, FormControlLabel, Checkbox, Radio } from '@material-ui/core'
+import { useStore } from '../../stores/RootStore/RootStoreContext'
 
 const RandomSongpageWrapper = styled.div`
   display: flex;
@@ -24,6 +26,10 @@ const SongFiltersTitle = styled.div`
   font-size: 38px;
   font-weight: bold;
 `
+const Divider = styled.hr`
+  margin: 2px;
+  border-color: rgba(0, 0, 0, 0.12);
+`
 const AlbumsFiltersBox = styled.div`
   padding: 20px;
   width: 18%;
@@ -33,13 +39,54 @@ const AlbumsFiltersBox = styled.div`
   box-shadow: 0 5px 15px rgb(0 0 0 / 8%);
   margin-left: 8px;
 `
+const FiltersContainer = styled.div``
+const FilterCategoryContainer = styled.div`
+  display: inline-block;
+  margin: 7px;
+`
+const FilterCategoryTitle = styled.div`
+  font-size: 31px;
+  font-weight: bold;
+  text-align: left;
+  margin-bottom: 13px;
+`
 
 export const RandomSongPage: FC = (): JSX.Element => {
+  const { TracksStore } = useStore()
+
+  const filters = TracksStore.filters.map((category) => {
+    return (
+      <FilterCategoryContainer key={category.name}>
+        <FilterCategoryTitle>{category.name}</FilterCategoryTitle>
+        {category.value !== 'extra' && (
+          <>
+            <RadioGroup />
+          </>
+        )}
+        {category.filters.map((filter) => {
+          return (
+            <>
+              {category.value === 'extra' ? (
+                <FormControlLabel labelPlacement='top' control={<Checkbox />} label={filter.title} />
+              ) : (
+                <>
+                  <FormControlLabel labelPlacement='top' control={<Radio />} label={filter.title} />
+                </>
+              )}
+            </>
+          )
+        })}
+      </FilterCategoryContainer>
+    )
+  })
+
   return (
     <RandomSongpageWrapper>
       <RandomSongPageContainer>
         <SongFiltersBox>
           <SongFiltersTitle>Выберите, какую песню хотите услышать</SongFiltersTitle>
+          <Divider />
+          <FiltersContainer>{filters}</FiltersContainer>
         </SongFiltersBox>
         <AlbumsFiltersBox></AlbumsFiltersBox>
       </RandomSongPageContainer>
