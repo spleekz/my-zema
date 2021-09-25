84b1303e3deaae4d2aa4a617633a7ec52bc005ad
spleekz <Shutnik-12K@yandex,ru>
2021-09-25 21:52:08 +0300

    add filters to tracks store

--- src/stores/TracksStore.ts
+++ src/stores/TracksStore.ts
@@ -1,8 +1,19 @@
 import { makeAutoObservable, runInAction } from 'mobx'
 import { ITrack, TracksApi } from '../API/TracksApi'
 
+interface IFilterCategory {
+  name: string
+  value: string
+  filters: Array<IFilter>
+}
+interface IFilter {
+  title: string
+  value: string
+  checked: boolean
+}
 export interface ITracksStore {
   tracks: Array<ITrack>
+  filters: Array<IFilterCategory>
   loadTracks(): void
 }
 
@@ -12,6 +23,34 @@ export class TracksStore implements ITracksStore {
   }
 
   tracks: Array<ITrack> = []
+  filters = [
+    {
+      name: 'Настроение',
+      value: 'mood',
+      filters: [
+        { title: 'Весёлое', value: 'happy', checked: false },
+        { title: 'Грустное', value: 'sad', checked: false },
+      ],
+    },
+    {
+      name: 'Темп',
+      value: 'tempo',
+      filters: [
+        { title: 'Быстрый', value: 'fast', checked: false },
+        { title: 'Медленный', value: 'slow', checked: false },
+      ],
+    },
+    {
+      name: 'Дополнительно',
+      value: 'extra',
+      filters: [
+        { title: 'Популярная', value: 'popular', checked: false },
+        { title: 'Запоминающиеся слова', value: 'words', checked: false },
+        { title: 'Запоминающаяся мелодия', value: 'melody', checked: false },
+        { title: 'Live', value: 'live', checked: false },
+      ],
+    },
+  ]
 
   loadTracks(): void {
     const accumTracks: Array<ITrack> = []
