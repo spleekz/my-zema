7630f1a1067c08d6c292576f742de425b99e2f25
spleekz <Shutnik-12K@yandex,ru>
2021-10-04 18:23:23 +0300

    add component for albums

---
+++ src/pages/RandomSong/ChooseAlbums.tsx
@@ -0,0 +1,48 @@
+import React, { FC } from 'react'
+import { Checkbox } from '@material-ui/core'
+import { observer } from 'mobx-react-lite'
+import styled from 'styled-components'
+import { useStore } from '../../stores/RootStore/RootStoreContext'
+
+interface AlbumContainerProps {
+  index: number
+}
+
+const AlbumContainer = styled.div<AlbumContainerProps>`
+  display: flex;
+  flex-direction: ${(props) => (props.index % 2 === 0 ? 'row-reverse' : 'row')};
+  justify-content: space-between;
+`
+const AlbumsFiltersBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px;
+  width: 18%;
+  background-color: #ffffff;
+  border-radius: 6px;
+  box-shadow: 0 5px 15px rgb(0 0 0 / 8%);
+  margin-left: 8px;
+`
+const AlbumPreview = styled.img`
+  width: 100px;
+  border-radius: 3px;
+`
+export const ChooseAlbums: FC = observer((): JSX.Element => {
+  const { TracksStore } = useStore()
+
+  return (
+    <AlbumsFiltersBox>
+      {TracksStore.albums
+        .filter((al) => al.id_album !== 814405)
+        .map((album, index) => {
+          return (
+            <AlbumContainer key={album.id_album} index={index}>
+              <Checkbox />
+              <AlbumPreview src={album.cover} />
+            </AlbumContainer>
+          )
+        })}
+    </AlbumsFiltersBox>
+  )
+})
--- src/pages/RandomSong/ChooseFilters.tsx
+++ src/pages/RandomSong/ChooseFilters.tsx
@@ -1,13 +1,7 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
 import { TrackFilters } from './TrackFilters'
-import { useStore } from '../../stores/RootStore/RootStoreContext'
-import { observer } from 'mobx-react-lite'
-import { Checkbox } from '@material-ui/core'
-
-interface AlbumContainerProps {
-  index: number
-}
+import { ChooseAlbums } from './ChooseAlbums'
 
 const ChooseFiltersPageContainer = styled.div`
   display: flex;
@@ -32,31 +26,10 @@ const Divider = styled.hr`
   margin: 2px;
   border-color: rgba(0, 0, 0, 0.12);
 `
-const AlbumsFiltersBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 20px;
-  width: 18%;
-  background-color: #ffffff;
-  border-radius: 6px;
-  box-shadow: 0 5px 15px rgb(0 0 0 / 8%);
-  margin-left: 8px;
-`
+
 const FiltersContainer = styled.div``
-const AlbumContainer = styled.div<AlbumContainerProps>`
-  display: flex;
-  flex-direction: ${(props) => (props.index % 2 === 0 ? 'row-reverse' : 'row')};
-  justify-content: space-between;
-`
-const AlbumPreview = styled.img`
-  width: 100px;
-  border-radius: 3px;
-`
 
-export const ChooseFilters: FC = observer((): JSX.Element => {
-  const { TracksStore } = useStore()
-
+export const ChooseFilters: FC = (): JSX.Element => {
   return (
     <ChooseFiltersPageContainer>
       <SongFiltersBox>
@@ -66,18 +39,7 @@ export const ChooseFilters: FC = observer((): JSX.Element => {
           <TrackFilters />
         </FiltersContainer>
       </SongFiltersBox>
-      <AlbumsFiltersBox>
-        {TracksStore.albums
-          .filter((al) => al.id_album !== 814405)
-          .map((album, index) => {
-            return (
-              <AlbumContainer key={album.id_album} index={index}>
-                <Checkbox />
-                <AlbumPreview src={album.cover} />
-              </AlbumContainer>
-            )
-          })}
-      </AlbumsFiltersBox>
+      <ChooseAlbums />
     </ChooseFiltersPageContainer>
   )
-})
+}
