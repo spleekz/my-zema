6c31b7cfe7da09232a4ace94c1b6e267dc341aa8
spleekz <Shutnik-12K@yandex,ru>
2021-10-29 03:12:09 +0300

    remake app store logic

--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -3,8 +3,10 @@ import isDarkColor from 'is-dark-color'
 
 export interface IAppStore {
   bodyBgc: string
+  defaultBodyBgc: string
   isBodyBgcDark: boolean
   setBodyBgc(color: string): void
+  setBodyBgcToDefault(): void
 }
 
 export class AppStore implements IAppStore {
@@ -12,13 +14,22 @@ export class AppStore implements IAppStore {
     makeAutoObservable(this)
   }
 
-  bodyBgc = '#f3f3f3'
+  bodyBgc: string
 
+  get defaultBodyBgc(): string {
+    return '#f3f3f3'
+  }
   get isBodyBgcDark(): boolean {
-    return isDarkColor(this.bodyBgc)
+    if (this.bodyBgc) {
+      return isDarkColor(this.bodyBgc)
+    }
+    return false
   }
 
   setBodyBgc(color: string): void {
     this.bodyBgc = color
+  }
+  setBodyBgcToDefault(): void {
+    this.bodyBgc = this.defaultBodyBgc
   }
 }
